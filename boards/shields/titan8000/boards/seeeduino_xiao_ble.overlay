d75eafae91ac92ecef87211af1325e59c2e1a0b3
Monksoffunk <monksoffunk@users.noreply.github.com>
2025-12-17 23:04:57 +0900

    chore(license): add monksoffunk to headers

--- boards/shields/titan8000/boards/seeeduino_xiao_ble.overlay
+++ boards/shields/titan8000/boards/seeeduino_xiao_ble.overlay
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2025 The ZMK Contributors
+ * Copyright (c) 2025 monksoffunk
  *
  * SPDX-License-Identifier: MIT
  */
--- config/drivers/kscan/kscan_gpio_charlieplex_titan8000.c
+++ config/drivers/kscan/kscan_gpio_charlieplex_titan8000.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020-2023 The ZMK Contributors
+ * Copyright (c) 2025 monksoffunk
  *
  * SPDX-License-Identifier: MIT
  */
